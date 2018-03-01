ea85475d2b8ae0d61a45366cc476fccd14dcac2a
Audrey Altman <audrey@dp.la>
2018-03-01 12:19:39 -0500

    refactor search results

--- components/shared/ListView/index.js
+++ components/shared/ListView/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Link from "next/link";
 
-import { joinIfArray } from "utilFunctions";
+import { joinIfArray, trackClickThrough } from "utilFunctions";
 import { truncateString } from "utilFunctions";
 
 import ListImage from "./ListImage";
 import { classNames, stylesheet } from "./ListView.css";
-
-import GaListViewWrapper from "./GaListViewWrapper";
 
 const externalLinkIcon = "/static/images/external-link-blue.svg";
 
@@ -25,66 +23,82 @@ const ItemDescription = ({ description }) => {
   );
 };
 
-const ListView = ({ items, route }) =>
-  <ul className={classNames.listView}>
-    {items.map(item =>
-      <li key={item["@id"] || item.id} className={classNames.listItem}>
-        <ListImage
-          item={item}
-          title={item.title}
-          type={item.type}
-          url={item.thumbnailUrl}
-          useDefaultImage={item.useDefaultImage}
-        />
-        <div className={classNames.itemInfo}>
-          <Link href={item.linkHref} as={item.linkAs}>
-            <a className={`classNames.listItemLink internalItemLink`}>
-              <h2 className={`hover-underline ${classNames.itemTitle}`}>
-                {route.pathname.indexOf("/search") === 0 && item.title
-                  ? truncateString(item.title, 150)
-                  : item.title}
-              </h2>
+const getGaEvent = item => {
+  const gaEvent = {
+    type: "Click Through",
+    itemId: item.id,
+    title: joinIfArray(item.title),
+    partner: joinIfArray(item.provider),
+    contributor: joinIfArray(item.dataProvider)
+  };
+  return gaEvent;
+};
+
+const ListView = ({ items, route }) => {
+  return (
+    <ul className={classNames.listView}>
+      {items.map(item =>
+        <li key={item["@id"] || item.id} className={classNames.listItem}>
+          <ListImage
+            item={item}
+            title={item.title}
+            type={item.type}
+            url={item.thumbnailUrl}
+            useDefaultImage={item.useDefaultImage}
+          />
+          <div className={classNames.itemInfo}>
+            <Link href={item.linkHref} as={item.linkAs}>
+              <a className={`classNames.listItemLink internalItemLink`}>
+                <h2 className={`hover-underline ${classNames.itemTitle}`}>
+                  {route.pathname.indexOf("/search") === 0 && item.title
+                    ? truncateString(item.title, 150)
+                    : item.title}
+                </h2>
+              </a>
+            </Link>
+
+            {(item.date || item.creator) &&
+              <span className={classNames.itemAuthorAndDate}>
+                {route.pathname.indexOf("/search") === 0 &&
+                  item.date &&
+                  <span>{item.date.displayDate}</span>}
+                {route.pathname.indexOf("/search") === 0 &&
+                  item.date &&
+                  item.date.displayDate &&
+                  item.creator &&
+                  <span> · </span>}
+                <span>{joinIfArray(item.creator, ", ")}</span>
+              </span>}
+            <ItemDescription description={item.description} />
+            <a
+              href={item.sourceUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={`hover-underline ${classNames.itemSource}`}
+              onClick={e =>
+                trackClickThrough(e, getGaEvent(item), item.sourceUrl)}
+            >
+              <span className={classNames.itemSourceText}>
+                {item.type === "image"
+                  ? "View Full Image"
+                  : item.type === "text" ? "View Full Text" : "View Full Item"}
+              </span>
+              <img
+                className={classNames.externalLinkIcon}
+                src={externalLinkIcon}
+                alt=""
+              />
             </a>
-          </Link>
+            {item.dataProvider &&
+              <span className={`${classNames.itemProvider}`}>
+                &nbsp; in {item.dataProvider}
+              </span>}
+          </div>
+        </li>
+      )}
+      <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+    </ul>
+  );
+};
 
-          {(item.date || item.creator) &&
-            <span className={classNames.itemAuthorAndDate}>
-              {route.pathname.indexOf("/search") === 0 &&
-                item.date &&
-                <span>{item.date.displayDate}</span>}
-              {route.pathname.indexOf("/search") === 0 &&
-                item.date &&
-                item.date.displayDate &&
-                item.creator &&
-                <span> · </span>}
-              <span>{joinIfArray(item.creator, ", ")}</span>
-            </span>}
-          <ItemDescription description={item.description} />
-          <a
-            href={item.sourceUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-            className={`hover-underline clickThrough ${classNames.itemSource}`}
-          >
-            <span className={classNames.itemSourceText}>
-              {item.type === "image"
-                ? "View Full Image"
-                : item.type === "text" ? "View Full Text" : "View Full Item"}
-            </span>
-            <img
-              className={classNames.externalLinkIcon}
-              src={externalLinkIcon}
-              alt=""
-            />
-          </a>
-          {item.dataProvider &&
-            <span className={`${classNames.itemProvider}`}>
-              &nbsp; in {item.dataProvider}
-            </span>}
-        </div>
-      </li>
-    )}
-    <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-  </ul>;
-
-export default GaListViewWrapper(ListView);
+export default ListView;
